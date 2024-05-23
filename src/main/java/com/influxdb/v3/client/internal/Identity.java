2487db0f735fb820c51160ce070868b1568cd3fb
karel rehor <karl.koerner@bonitoo.io>
2024-05-23 14:49:57 +0200

    chore: change access level of new class

--- src/main/java/com/influxdb/v3/client/internal/Identity.java
+++ src/main/java/com/influxdb/v3/client/internal/Identity.java
@@ -24,14 +24,14 @@ package com.influxdb.v3.client.internal;
 /**
  * Functions for establishing caller identity.
  */
-public final class Identity {
+final class Identity {
   private Identity() { }
 
   /**
    * Attempt to get the package version.
    * @return - package version or unknown.
    */
-  public static String getVersion() {
+  static String getVersion() {
     Package mainPackage = Identity.class.getPackage();
     String version = mainPackage != null ? mainPackage.getImplementationVersion() : "unknown";
     return version == null ? "unknown" : version;
@@ -41,7 +41,7 @@ public final class Identity {
    * Get a standard user-agent identity to be used in all HTTP based calls.
    * @return - the standard user-agent string.
    */
-  public static String getUserAgent() {
+  static String getUserAgent() {
     return String.format("influxdb3-java/%s", getVersion());
   }
 }
