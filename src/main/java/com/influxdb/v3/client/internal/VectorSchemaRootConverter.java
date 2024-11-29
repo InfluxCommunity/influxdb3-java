547c87e433aace452710bbdeb32f258c4cff2e2c
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2024-11-29 14:40:26 +0700

    fix: linter

--- src/main/java/com/influxdb/v3/client/internal/VectorSchemaRootConverter.java
+++ src/main/java/com/influxdb/v3/client/internal/VectorSchemaRootConverter.java
@@ -166,13 +166,13 @@ public final class VectorSchemaRootConverter {
                         return value;
                     }
                 default:
+                    return value;
             }
         } else if ("timestamp".equals(valueType) || Objects.equals(fieldName, "time")) {
             return NanosecondConverter.getTimestampNano(value, field);
         } else {
             return TypeCasting.toStringValue(value);
         }
-        return value;
     }
 
     /**
