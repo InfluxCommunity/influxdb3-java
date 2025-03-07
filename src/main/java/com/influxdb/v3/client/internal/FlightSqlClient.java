f859de9274e06a410398e65c02ef1fd158318392
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2025-03-07 17:16:06 +0700

    fix: linter

--- src/main/java/com/influxdb/v3/client/internal/FlightSqlClient.java
+++ src/main/java/com/influxdb/v3/client/internal/FlightSqlClient.java
@@ -31,7 +31,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.stream.Stream;
