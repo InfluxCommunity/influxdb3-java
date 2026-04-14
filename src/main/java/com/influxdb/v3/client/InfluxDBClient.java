2a1d436de25023a981a45351cbc8fee80fa6ec91
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2026-04-14 14:35:07 +0700

    docs: comments for write null fields

--- src/main/java/com/influxdb/v3/client/InfluxDBClient.java
+++ src/main/java/com/influxdb/v3/client/InfluxDBClient.java
@@ -78,7 +78,11 @@ public interface InfluxDBClient extends AutoCloseable {
      * @param point the {@link Point} to write, can be null
      * <p>
      * Note: the timestamp passed will be converted to nanoseconds since the Unix epoch
-     * by NanosecondConverter helper class
+     * by NanosecondConverter helper class. <br>
+     * Warning: If you try to write ONLY ONE Point to Influxdb, but that Point has fields with null values,
+     * then these fields will NOT be written to Influxdb.
+     * In the following queries If you explicitly query these null fields like this: "Select normalField, nullField from my_table"
+     * error will be thrown.
      */
     void writePoint(@Nullable final Point point);
 
@@ -89,7 +93,13 @@ public interface InfluxDBClient extends AutoCloseable {
      * @param options the options for writing data to InfluxDB
      * <p>
      * Note: the timestamp passed will be converted to nanoseconds since the Unix epoch
-     * by NanosecondConverter helper class
+     * by NanosecondConverter helper class <br>
+     * Note: the timestamp passed will be converted to nanoseconds since the Unix epoch
+     * by NanosecondConverter helper class. <br>
+     * Warning: If you try to write ONLY ONE Point to Influxdb, but that Point has fields with null values,
+     * then these fields will NOT be written to Influxdb.
+     * In the following queries If you explicitly query these null fields like this: "Select normalField, nullField from my_table"
+     * error will be thrown.
      */
     void writePoint(@Nullable final Point point, @Nonnull final WriteOptions options);
 
@@ -99,7 +109,13 @@ public interface InfluxDBClient extends AutoCloseable {
      * @param points the list of {@link Point} to write, cannot be null
      * <p>
      * Note: the timestamp passed will be converted to nanoseconds since the Unix epoch
-     * by NanosecondConverter helper class
+     * by NanosecondConverter helper class <br>
+     * Note: the timestamp passed will be converted to nanoseconds since the Unix epoch
+     * by NanosecondConverter helper class. <br>
+     * Warning: If you try to write ONLY ONE Point to Influxdb, but that Point has fields with null values,
+     * then these fields will NOT be written to Influxdb.
+     * In the following queries If you explicitly query these null fields like this: "Select normalField, nullField from my_table"
+     * error will be thrown.
      */
     void writePoints(@Nonnull final List<Point> points);
 
@@ -111,6 +127,12 @@ public interface InfluxDBClient extends AutoCloseable {
      * <p>
      * Note: the timestamp passed will be converted to nanoseconds since the Unix epoch
      * by NanosecondConverter helper class
+     * Note: the timestamp passed will be converted to nanoseconds since the Unix epoch
+     * by NanosecondConverter helper class. <br>
+     * Warning: If you try to write ONLY ONE Point to Influxdb, but that Point has fields with null values,
+     * then these fields will NOT be written to Influxdb.
+     * In the following queries If you explicitly query these null fields like this: "Select normalField, nullField from my_table"
+     * error will be thrown.
      */
     void writePoints(@Nonnull final List<Point> points, @Nonnull final WriteOptions options);
 
